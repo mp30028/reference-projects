a87101c90f2fcb4bc908e7993d0eb00c9f06485e
mp30028 <mp30028@gmail.com>
2024-05-17 10:59:00 +0100

    created barebones spring boot help

---
+++ java/spring-boot-apps/hello-world-barebones/src/test/java/com/zonesoft/reference_projects/hello_world_barebones/HelloWorldBarebonesAppTests.java
@@ -0,0 +1,13 @@
+package com.zonesoft.reference_projects.hello_world_barebones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldBarebonesAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
