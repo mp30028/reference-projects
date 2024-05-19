51c59a640478c93299b86e6e61f06fc369a29ee2
mp30028 <mp30028@gmail.com>
2024-05-19 09:55:02 +0100

    loki based logging configured in ui app

--- java/spring-boot-apps/03-observability/hello-world-modular/hello-world-ui/src/main/java/com/zonesoft/reference/ui/hello_world/HelloWorldUiApp.java
+++ java/spring-boot-apps/03-observability/hello-world-modular/hello-world-ui/src/main/java/com/zonesoft/reference/ui/hello_world/HelloWorldUiApp.java
@@ -1,12 +1,17 @@
 package com.zonesoft.reference.ui.hello_world;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldUiApp {
-
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldUiApp.class);
+	
 	public static void main(String[] args) {
+		LOGGER.debug("Application Started with args={}", args.toString());
 		SpringApplication.run(HelloWorldUiApp.class, args);
 	}
 
