09f675c17c238a739421113d0aaea4449fb1109c
mp30028 <mp30028@gmail.com>
2024-05-19 06:26:53 +0100

    copy 02-multi-module to 03-observability

---
+++ java/spring-boot-apps/03-observability/hello-world-modular/hello-world-services/calendar-service/src/main/java/com/zonesoft/reference/services/calendar/CalendarServiceApp.java
@@ -0,0 +1,13 @@
+package com.zonesoft.reference.services.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarServiceApp.class, args);
+	}
+
+}
