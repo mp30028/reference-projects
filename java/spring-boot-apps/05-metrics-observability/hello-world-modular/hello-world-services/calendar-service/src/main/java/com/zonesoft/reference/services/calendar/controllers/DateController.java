62c9fcbeb4fceb13de871c3fa1d93477bd8e1fc4
mp30028 <mp30028@gmail.com>
2024-05-22 14:12:47 +0100

    update trial metrics generation and capture

--- java/spring-boot-apps/05-metrics-observability/hello-world-modular/hello-world-services/calendar-service/src/main/java/com/zonesoft/reference/services/calendar/controllers/DateController.java
+++ java/spring-boot-apps/05-metrics-observability/hello-world-modular/hello-world-services/calendar-service/src/main/java/com/zonesoft/reference/services/calendar/controllers/DateController.java
@@ -3,6 +3,8 @@ package com.zonesoft.reference.services.calendar.controllers;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,34 +14,48 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zonesoft.reference.utils.DoNothing;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+
 @RestController
 
 public class DateController {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(DateController.class);
-//	private static final int MINIMUM_WAIT_MS =30;
-//	private static final int MAXIMUM_WAIT_MS = 2100;
+	
+	private final Counter countOfTotalGetDateRequests;
+	private final Timer timeTakenToProcessGetDateRequest;
+	
+	public DateController(MeterRegistry registry) {
+		
+		this.countOfTotalGetDateRequests = Counter
+				.builder("getDate_invoked_total")
+				.tag("request", "get-date")
+				.description("Total number of times get-date api was called")
+				.register(registry);
+		
+		this.timeTakenToProcessGetDateRequest = Timer
+				.builder("getDate_processing_time")
+				.tag("request", "get-date")
+				.description("Time taken to process a get-date api call")
+				.register(registry);
+	}
+	
 	
 	@GetMapping(value={"/get-date"})
 	@ResponseBody
 	public ResponseEntity<String> getDate() {
-		Instant gmtInstant = Instant.now();
-		String PATTERN_FORMAT = "dd-MMM-yyyy";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN_FORMAT).withZone(ZoneId.of("UTC"));
-        String formattedInstant = formatter.format(gmtInstant);
-        LOGGER.debug("getDate() invoked. gmtInstant={}, PATTERN_FORMAT={}, formattedInstant={})", gmtInstant, PATTERN_FORMAT, formattedInstant);
-        DoNothing.pretendDoingSomething();;
+		long start = System.currentTimeMillis();
+			countOfTotalGetDateRequests.increment();
+			Instant gmtInstant = Instant.now();
+			String PATTERN_FORMAT = "dd-MMM-yyyy";
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN_FORMAT).withZone(ZoneId.of("UTC"));
+	        String formattedInstant = formatter.format(gmtInstant);
+	        LOGGER.debug("getDate() invoked. gmtInstant={}, PATTERN_FORMAT={}, formattedInstant={})", gmtInstant, PATTERN_FORMAT, formattedInstant);
+	        DoNothing.pretendDoingSomething();
+        timeTakenToProcessGetDateRequest.record(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
 		return ResponseEntity.ok(formattedInstant);
 	}
 	
-//	private void pretendDoingSomething() {
-//        try {
-//        	int waitMilliseconds = new Random().nextInt(MAXIMUM_WAIT_MS - MINIMUM_WAIT_MS + 1) + MINIMUM_WAIT_MS;
-//        	LOGGER.debug("Starting waitMilliseconds={}", waitMilliseconds);
-//            Thread.sleep(waitMilliseconds);
-//            LOGGER.debug("Finished waitMilliseconds={}", waitMilliseconds);
-//        } catch (InterruptedException e) {
-//            System.err.format("IOException: %s%n", e);
-//        }
-//	}
 }
