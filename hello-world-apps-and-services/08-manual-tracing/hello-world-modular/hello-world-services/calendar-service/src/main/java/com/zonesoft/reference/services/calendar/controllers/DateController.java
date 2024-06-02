51426751b978ffa099dbc615b68f8a6bef52ac3b
mp30028 <mp30028@gmail.com>
2024-06-02 09:01:49 +0100

    added instrumentation to trace internal library calls

--- hello-world-apps-and-services/08-manual-tracing/hello-world-modular/hello-world-services/calendar-service/src/main/java/com/zonesoft/reference/services/calendar/controllers/DateController.java
+++ hello-world-apps-and-services/08-manual-tracing/hello-world-modular/hello-world-services/calendar-service/src/main/java/com/zonesoft/reference/services/calendar/controllers/DateController.java
@@ -7,6 +7,7 @@ import java.time.format.DateTimeFormatter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zonesoft.reference.utils.DoNothing;
 
+import io.opentelemetry.api.OpenTelemetry;
+
 //import io.micrometer.core.instrument.Counter;
 //import io.micrometer.core.instrument.MeterRegistry;
 //import io.micrometer.core.instrument.Timer;
 
 @RestController
-
 public class DateController {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(DateController.class);
+	private final OpenTelemetry openTelemetry;
+	
+	@Autowired
+	public DateController(OpenTelemetry openTelemetry) {
+		super();
+		this.openTelemetry = openTelemetry;
+	}
 	
 //	private final Counter countOfTotalGetDateRequests;
 //	private final Timer timeTakenToProcessGetDateRequest;
@@ -53,7 +62,7 @@ public class DateController {
 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN_FORMAT).withZone(ZoneId.of("UTC"));
 	        String formattedInstant = formatter.format(gmtInstant);
 	        LOGGER.debug("getDate() invoked. gmtInstant={}, PATTERN_FORMAT={}, formattedInstant={})", gmtInstant, PATTERN_FORMAT, formattedInstant);
-	        DoNothing.pretendDoingSomething();
+	        new DoNothing(this.openTelemetry).pretendDoingSomething();
 //        timeTakenToProcessGetDateRequest.record(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
 		return ResponseEntity.ok(formattedInstant);
 	}
