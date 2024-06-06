de6704324f13b63bfbdb56c4a335f1ab9efac848
mp30028 <mp30028@gmail.com>
2024-06-06 14:35:37 +0100

    create seeded project directory

---
+++ 05-spring-boot-security/[a]-unsecured-apps/hello-world-modular/hello-world-services/calendar-service/src/test/java/com/zonesoft/reference/services/calendar/CalendarServiceAppTests.java
@@ -0,0 +1,13 @@
+package com.zonesoft.reference.services.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServiceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
