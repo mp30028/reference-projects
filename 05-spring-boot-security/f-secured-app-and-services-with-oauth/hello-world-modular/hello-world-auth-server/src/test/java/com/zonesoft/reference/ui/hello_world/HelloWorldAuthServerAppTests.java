5bf0f4eca9bf6a9a28fc69c8be1b78678e17d44c
mp30028 <mp30028@gmail.com>
2024-06-24 10:52:01 +0100

    new seeded project with authorization server created

---
+++ 05-spring-boot-security/f-secured-app-and-services-with-oauth/hello-world-modular/hello-world-auth-server/src/test/java/com/zonesoft/reference/ui/hello_world/HelloWorldAuthServerAppTests.java
@@ -0,0 +1,13 @@
+package com.zonesoft.reference.ui.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAuthServerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
