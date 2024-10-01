e4e005551d62185a4bd3cdc7a5cd549b40f0939b
mp30028 <mp30028@gmail.com>
2024-10-02 00:58:22 +0100

    project with context path and default routing added

---
+++ 08-gway-exampes/c-routing-context-path/hello-world-suite/hello-world-ui/src/main/java/com/zonesoft/reference/ui/hello_world/HelloWorldUiApp.java
@@ -0,0 +1,30 @@
+package com.zonesoft.reference.ui.hello_world;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldUiApp {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldUiApp.class);
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(HelloWorldUiApp.class, args);
+		LOGGER.debug("Application Started with args={}", Arrays.toString(args));
+	}
+
+//	@Configuration
+//	public class WebMvcConfig implements WebMvcConfigurer  {
+//
+//	    @Override
+//	    public void configurePathMatch(PathMatchConfigurer configurer) {
+//	        configurer.addPathPrefix("api", HandlerTypePredicate.forAnnotation(RestController.class));
+//	    }
+//	}	
+	
+}
